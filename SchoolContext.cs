663f51432aaaec9cf6c045904f0b07f78d2284d5
ufukkskc <150153686+ufukkskc@users.noreply.github.com>
2025-06-24 11:24:38 -0400

    Add files via upload

---
+++ SchoolContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Data.Entity;
+
+namespace EFCodeFirstStudentApp
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext() : base("SchoolDBConnectionString")
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
